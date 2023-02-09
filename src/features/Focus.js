9ff72251d5816650204718967fe9b789cee208e0
ewldev <edwin.leung2@gmail.com>
2023-02-09 13:28:57 -0500

    adding a button and styling

--- src/features/Focus.js
+++ src/features/Focus.js
@@ -1,27 +1,41 @@
-
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { TextInput } from 'react-native-paper';
 import { colors } from '../utils/colors';
+import { RoundedButton } from '../components/RoundedButton';
 
 export const Focus = () => {
-    const [subject, setSubject] = useState(null)
-    return (
-      <View style={styles.container}>
-        <View style={styles.inputContainer}>
-          <TextInput onChangeText={setSubject} label="What task would you like to add?" />
+  const [subject, setSubject] = useState(null);
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={setSubject}
+          label="What task would you like to add?"
+        />
+        <View style={styles.button}>
+          <RoundedButton title="+" size={50} />
         </View>
       </View>
-    );
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  button: {
+    justifyContent: 'center',
+  },
+  textInput: {
+    flex: 1,
+    marginRight: 10,
+  },
   inputContainer: {
-    flex: 0.5,
-    padding: 50,
+    padding: 25,
     justifyContent: 'top',
-  }, 
+    flexDirection: 'row',
+  },
 });
